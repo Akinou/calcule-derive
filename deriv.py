718b381633ab3587cd2b116fb20642d80f807caf
Akan <62407014+Akinou@users.noreply.github.com>
2023-03-30 14:29:30 +0200

    Update deriv.py

--- deriv.py
+++ deriv.py
@@ -1,1 +1,31 @@
 
+def u(x):
+    """Définition de la fonction u"""
+    return x ** 2
+
+def v(x):
+    """Définition de la fonction v"""
+    return 3 * x
+
+def produit(u, v):
+    """Définition du produit u*v"""
+    return u * v
+
+def quotient(u, v):
+    """Définition du quotient u/v"""
+    return u / v
+
+def derivee_produit(u, v, x):
+    """Calcule la dérivée du produit u*v en x"""
+    return u(x) * v.deriv()(x) + v(x) * u.deriv()(x)
+
+def derivee_quotient(u, v, x):
+    """Calcule la dérivée du quotient u/v en x"""
+    return (v(x) * u.deriv()(x) - u(x) * v.deriv()(x)) / v(x) ** 2
+
+# Exemple d'utilisation :
+x = 2
+f = lambda x: produit(u, v)(x) / quotient(u, v)(x)
+derivee_f = f.deriv()(x)
+
+print(f"La dérivée de la fonction f(x) = u(x)*v(x) / (u(x)/v(x)) en x = {x} est {derivee_f}")
