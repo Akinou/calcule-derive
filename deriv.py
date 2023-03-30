c09efb69a9c4d6e4527a2aca59cf96d000c7a97f
Akan <62407014+Akinou@users.noreply.github.com>
2023-03-30 14:25:57 +0200

    Update deriv.py

--- deriv.py
+++ deriv.py
@@ -1,13 +1,1 @@
-def f(x):
-    """Définition de la fonction à dériver"""
-    return x ** 2
 
-def derivee(f, x, h=1e-6):
-    """Calcule la dérivée de la fonction f en x à l'aide de la méthode de différenciation numérique"""
-    return (f(x + h) - f(x - h)) / (2 * h)
-
-# Exemple d'utilisation :
-x = 2
-derivee_f = derivee(f, x)
-
-print(f"La dérivée de la fonction f(x) = x^2 en x = {x} est {derivee_f}")
